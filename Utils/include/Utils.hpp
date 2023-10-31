90058ffffead5f0e5c2a15a42fe259c8fe0e4200
bhoh <bhoh@mit.edu>
2023-10-31 00:29:30 -0400

    moved parser to new repo

--- Utils/include/Utils.hpp
+++ Utils/include/Utils.hpp
@@ -5,21 +5,6 @@
 #include <algorithm>
 #include <cmath>
 #include <string>
-
-enum TokenType {
-    NUMBER,
-    OPERATOR,
-    OPEN_PAREN,
-    CLOSE_PAREN,
-    INDEP_VARIABLE,
-    MATH_FUNCTION
-    // Add more categories as needed
-};
-
-struct Token {
-    TokenType type;
-    std::string value;
-};
  
 namespace Utils {
     std::vector<double> evalLagrangeInterp(int k, std::vector<double> evalPoints, std::vector<double> &gaussPoints);
@@ -27,7 +12,4 @@ namespace Utils {
     std::vector<double> numDeriv(double h, int k, std::vector<double> &evalPoints, std::vector<double> &gaussPoints);
     std::vector<double> integrateLagrange(std::vector<double> &gaussPoints);
     std::vector<double> ReshuffleNodeVals(std::vector<int> RmOrder, std::vector<int> CwOrder, std::vector<double> shuffleArray);
-    std::vector<double> ParseExpression(std::string inputString);
-    std::vector<Token> TokenizeString(std::string &inputString);
-    std::vector<Token> ShuntingYard(std::vector<Token> &tokens);
 }
